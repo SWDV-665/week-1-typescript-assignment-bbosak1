6f43474f7e9e08ad95d382c7495afaa33e569883
bbosak1 <bbosak1@live.maryville.edu>
2020-07-04 16:30:47 -0500

    Add price attribute

--- grocery.js
+++ grocery.js
@@ -1,26 +1,27 @@
 var Grocery = /** @class */ (function () {
-    function Grocery(name, quantity) {
+    function Grocery(name, quantity, price) {
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
     }
     return Grocery;
 }());
 function createGroceries(groceryList) {
-    var returnStatement = 'My grocery list: ';
+    var returnStatement = 'My grocery list:';
     for (var _i = 0, groceryList_1 = groceryList; _i < groceryList_1.length; _i++) {
         var element = groceryList_1[_i];
-        returnStatement = returnStatement + element.name + ". Quantity: " + element.quantity + " ";
+        returnStatement = returnStatement + " Item: " + element.name + ". Quantity: " + element.quantity + ". Price: " + element.price;
     }
     return returnStatement;
 }
 var groceryList = [];
-var banana = new Grocery("banana", 3);
-var bread = new Grocery("bread", 5);
-var eggs = new Grocery("eggs", 30);
-var milk = new Grocery("milk", 4);
-var cheese = new Grocery("cheese", 1);
-var meat = new Grocery("watermeatmelon", 1);
-var pasta = new Grocery("pasta", 7);
+var banana = new Grocery("banana", 3, 0.35);
+var bread = new Grocery("bread", 5, 2.25);
+var eggs = new Grocery("eggs", 30, 0.99);
+var milk = new Grocery("milk", 4, 1.79);
+var cheese = new Grocery("cheese", 1, 3.50);
+var meat = new Grocery("watermeatmelon", 1, 3.50);
+var pasta = new Grocery("pasta", 7, 2.40);
 groceryList.push(banana);
 groceryList.push(bread);
 groceryList.push(eggs);
--- grocery.ts
+++ grocery.ts
@@ -1,27 +1,28 @@
 class Grocery {
   constructor(
     public name: string,
-    public quantity: number
+    public quantity: number,
+    public price: number,
   ) {
   }
 }
 
 function createGroceries(groceryList) {
-  let returnStatement = 'My grocery list: ';
+  let returnStatement = 'My grocery list:';
   for (let element of groceryList) {
-    returnStatement = returnStatement + element.name + ". Quantity: " + element.quantity + " ";
+    returnStatement = returnStatement + " Item: " + element.name + ". Quantity: " + element.quantity + ". Price: " + element.price;
   }
   return returnStatement;
 }
 
 const groceryList = [];
-let banana = new Grocery("banana",3);
-let bread = new Grocery("bread",5);
-let eggs = new Grocery("eggs",30);
-let milk = new Grocery("milk",4);
-let cheese = new Grocery("cheese",1);
-let meat = new Grocery("watermeatmelon",1);
-let pasta = new Grocery("pasta",7);
+let banana = new Grocery("banana",3,0.35);
+let bread = new Grocery("bread",5,2.25);
+let eggs = new Grocery("eggs",30,0.99);
+let milk = new Grocery("milk",4,1.79);
+let cheese = new Grocery("cheese",1,3.50);
+let meat = new Grocery("watermeatmelon",1,3.50);
+let pasta = new Grocery("pasta",7,2.40);
 groceryList.push(banana);
 groceryList.push(bread);
 groceryList.push(eggs);
